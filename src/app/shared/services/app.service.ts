c1329fe8b05c5b011d7c43c970ec18472683c348
kostisgtr <kostisgtr@gmail.com>
2019-11-21 18:00:15 +0200

    fixed TMF APIURL read from config file (APITMFURL property)

--- src/app/shared/services/app.service.ts
+++ src/app/shared/services/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BootstrapService } from 'src/app/bootstrap/bootstrap.service';
+import { ApiConfiguration } from 'src/app/openApis/ServiceCatalogManagement/api-configuration'
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,10 @@ export class AppService {
 
   constructor(
     private bootstrapService: BootstrapService,
-  ) { }
+    private tmfAPIconfig: ApiConfiguration
+  ) { 
+    this.tmfAPIconfig.rootUrl = this.config.APITMFURL
+  }
 
   config = this.bootstrapService.getConfig()
 }

f8e55db39cba1a31434111284eb90800085a61de
kostisgtr <kostisgtr@gmail.com>
2019-12-02 22:49:51 +0200

    Default OAuth config file changes

--- src/assets/config/config.oauth.default.ts
+++ src/assets/config/config.oauth.default.ts
@@ -3,21 +3,27 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 export const authConfig: AuthConfig = {
 
   // Url of the Identity Provider
-  issuer: 'http://steyer-identity-server.azurewebsites.net/identity',
+  issuer: 'http://portal.openslice.io/osapi-oauth-server/oath/authorize',
+
+  loginUrl: 'http://portal.openslice.io/osapi-oauth-server/login',
+
+  // tokenEndpoint: 'http://portal.openslice.io/osapi-oauth-server/oath/token',
 
   // URL of the SPA to redirect the user to after login
   redirectUri: window.location.origin + '/services/services_marketplace',
 
   // The SPA's id. The SPA is registered with this id at the auth-server
-  clientId: 'spa-demo',
+  clientId: 'osapiWebClientId',
 
   dummyClientSecret: 'secret',
 
   responseType: 'code',
 
+  requireHttps: false,
+
   // set the scope for the permissions the client should request
   // The first three are defined by OIDC. The 4th is a usecase-specific one
-  scope: 'openid profile email voucher',
+  scope: 'read write admin openapi',
 
   showDebugInformation: true,
 }

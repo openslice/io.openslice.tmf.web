d17e731e9fadd90dfcaa7bdddaade5f3154da79f
kostistr <kostisgtr@gmail.com>
2019-12-23 12:57:50 +0200

    APIs RootURLs dynamically set by config files

--- src/app/shared/services/app.service.ts
+++ src/app/shared/services/app.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { BootstrapService } from 'src/app/bootstrap/bootstrap.service';
-import { ApiConfiguration } from 'src/app/openApis/ServiceCatalogManagement/api-configuration'
+import { ApiConfiguration as PortalAPIconfig} from 'src/app/openApis/PortalRepositoryAPI/api-configuration'
+import { ApiConfiguration as ServiceCatalogAPIconfig} from 'src/app/openApis/ServiceCatalogManagement/api-configuration';
+import { ApiConfiguration as ServiceInventoryAPIconfig} from 'src/app/openApis/ServiceInventoryManagement/api-configuration'
+import { ApiConfiguration as ServiceOrderingAPIconfig} from 'src/app/openApis/ServiceOrderingManagement/api-configuration'
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +12,15 @@ export class AppService {
 
   constructor(
     private bootstrapService: BootstrapService,
-    private tmfAPIconfig: ApiConfiguration
+    private portalAPIConfig: PortalAPIconfig,
+    private tmfServiceCatalogConfig: ServiceCatalogAPIconfig,
+    private tmfServiceInventoryConfig: ServiceInventoryAPIconfig,
+    private tmfServiceOrderingConfig: ServiceOrderingAPIconfig,
   ) { 
-    this.tmfAPIconfig.rootUrl = this.config.APITMFURL
+    this.portalAPIConfig.rootUrl = this.config.APIURL
+    this.tmfServiceCatalogConfig.rootUrl = this.config.APITMFURL
+    this.tmfServiceOrderingConfig.rootUrl = this.config.APITMFURL
+    this.tmfServiceInventoryConfig.rootUrl = this.config.APITMFURL    
   }
 
   config = this.bootstrapService.getConfig()

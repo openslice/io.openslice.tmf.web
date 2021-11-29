62478d3eade8e8e7ff95c5c367ba37648a5f5f00
Panagiotis Kavadias <p.kavvadias@upnet.gr>
2021-11-29 20:16:24 +0200

    Resource catalog openapi init

---
+++ src/app/openApis/ResourceCatalogManagement/models/url.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+export interface URL {
+  authority?: string;
+  content?: {};
+  defaultPort?: number;
+  file?: string;
+  host?: string;
+  path?: string;
+  port?: number;
+  protocol?: string;
+  query?: string;
+  ref?: string;
+  userInfo?: string;
+}

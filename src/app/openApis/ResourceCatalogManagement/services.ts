15c01ec9aa46897edcf58843e00d23bdd1ad5f07
Panagiotis Kavadias <p.kavvadias@upnet.gr>
2021-12-19 18:28:43 +0200

    Code cleanup

--- src/app/openApis/ResourceCatalogManagement/services.ts
+++ src/app/openApis/ResourceCatalogManagement/services.ts
@@ -1,7 +1,3 @@
-// export { ExportJobService } from './services/export-job.service';
-// export { EventsSubscriptionService } from './services/events-subscription.service';
-// export { ImportJobService } from './services/import-job.service';
-// export { NotificationListenersClientSideService } from './services/notification-listeners-client-side.service';
 export { ResourceCandidateService } from './services/resource-candidate.service';
 export { ResourceCatalogService } from './services/resource-catalog.service';
 export { ResourceCategoryService } from './services/resource-category.service';
